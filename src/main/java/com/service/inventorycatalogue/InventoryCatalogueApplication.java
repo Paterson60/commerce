3c883696e93bd6676a81406408e75c7c5f05b34b
paterson4_deloitte <paterson4@deloitte.com>
2024-08-03 00:22:35 +0530

    first-push

--- src/main/java/com/service/inventorycatalogue/InventoryCatalogueApplication.java
+++ src/main/java/com/service/inventorycatalogue/InventoryCatalogueApplication.java
@@ -2,12 +2,14 @@ package com.service.inventorycatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 //@EnableEurekaClient
+@EnableDiscoveryClient
 public class InventoryCatalogueApplication {
 
 	public static void main(String[] args) {
